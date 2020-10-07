fd45cec3eb988618c54b31d6a46689afd76e9b51
MarceloCLopes <marc01mcl@gmail.com>
2020-10-07 13:33:00 -0300

    Subir dinossauro

--- script.js
+++ script.js
@@ -3,8 +3,33 @@ const dino = document.querySelector(".dino");
 //função para pressionar a barra de espaço
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    console.log("pressionou espaço!");
+    jump();
   }
+}
+
+//função para o dino pular
+function jump() {
+  let position = 0;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+
+      //Descendo
+      let downInterval = setInterval(() => {
+        if (position < 0) {
+          clearInterval(downInterval);
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      //Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
